a14c09348b81eebf2281e5dfd049402e8a41a4ec
Siddhant Agarwal <sidaxy@gmail.com>
2024-09-08 15:59:29 +0530

    Create graph_build.py

---
+++ ch5/graph_build.py
@@ -0,0 +1,208 @@
+import os
+from neo4j import GraphDatabase
+from dotenv import load_dotenv
+import warnings
+
+warnings.filterwarnings("ignore")
+load_dotenv()
+
+class CreateGraph:
+
+    def __init__(self, uri, user, password, database='neo4j'):
+        self.driver = GraphDatabase.driver(uri, auth=(user, password), database=database)
+
+    def close(self):
+        self.driver.close()
+
+    def create_constraints(self):
+        queries = [
+            "CREATE CONSTRAINT unique_movie_id IF NOT EXISTS FOR (m:Movie) REQUIRE m.tmdbId IS NODE KEY;",
+            "CREATE CONSTRAINT unique_person_id IF NOT EXISTS FOR (p:Person) REQUIRE p.person_id IS NODE KEY;",
+            "CREATE CONSTRAINT unique_prod_id IF NOT EXISTS FOR (p:ProductionCompany) REQUIRE p.company_id IS NODE KEY;",
+            "CREATE CONSTRAINT unique_genre_id IF NOT EXISTS FOR (g:Genre) REQUIRE g.genre_id IS NODE KEY;",
+            "CREATE CONSTRAINT unique_lang_id IF NOT EXISTS FOR (l:SpokenLanguage) REQUIRE l.language_code IS NODE KEY;",
+            "CREATE CONSTRAINT unique_country_id IF NOT EXISTS FOR (c:Country) REQUIRE c.country_code IS NODE KEY;"
+        ]
+        with self.driver.session() as session:
+            for query in queries:
+                session.run(query)
+            print("Constraints created successfully.")
+
+    def load_movies(self, csv_file, limit):
+        query = f"""
+        LOAD CSV WITH HEADERS FROM $csvFile AS row
+        WITH row, toInteger(row.tmdbId) AS tmdbId
+        WHERE tmdbId IS NOT NULL
+        WITH row, tmdbId
+        LIMIT {limit}
+        MERGE (m:Movie {{tmdbId: tmdbId}})
+        ON CREATE SET m.title = coalesce(row.title, "None"),
+                      m.original_title = coalesce(row.original_title, "None"),
+                      m.imdb_id = coalesce(row.imdb_id, "None"),
+                      m.adult = CASE 
+                                    WHEN toInteger(row.adult) = 1 THEN 'Yes' 
+                                    ELSE 'No' 
+                                END,
+                      m.budget = toInteger(coalesce(row.budget, 0)),
+                      m.original_language = coalesce(row.original_language, "None"),
+                      m.revenue = toInteger(coalesce(row.revenue, 0)),
+                      m.tagline = coalesce(row.tagline, "None"),
+                      m.overview = coalesce(row.overview, "None"),
+                      m.release_date = coalesce(row.release_date, "None"),
+                      m.runtime = toFloat(coalesce(row.runtime, 0)),
+                      m.belongs_to_collection = coalesce(row.belongs_to_collection, "None");
+        """
+        with self.driver.session() as session:
+            session.run(query, csvFile=f'{csv_file}')
+            print(f"Movies loaded from {csv_file} (limited to {limit} entries)")
+
+    def load_genres(self, csv_file):
+        query = """
+        LOAD CSV WITH HEADERS FROM $csvFile AS row
+        MATCH (m:Movie {tmdbId: toInteger(row.tmdbId)})  // Check if the movie exists
+        WITH m, row
+        MERGE (g:Genre {genre_id: toInteger(row.genre_id)})
+        ON CREATE SET g.genre_name = row.genre_name
+        MERGE (m)-[:HAS_GENRE]->(g);
+        """
+        with self.driver.session() as session:
+            session.run(query, csvFile=f'{csv_file}')
+            print(f"Genres and relationships to movies loaded from {csv_file}")
+
+    def load_production_companies(self, csv_file):
+        query = """
+        LOAD CSV WITH HEADERS FROM $csvFile AS row
+        MATCH (m:Movie {tmdbId: toInteger(row.tmdbId)})  // Check if the movie exists
+        WITH m, row
+        MERGE (pc:ProductionCompany {company_id: toInteger(row.company_id)})
+        ON CREATE SET pc.company_name = row.company_name
+        MERGE (m)-[:PRODUCED_BY]->(pc);
+        """
+        with self.driver.session() as session:
+            session.run(query, csvFile=f'{csv_file}')
+            print(f"Production companies and relationships to movies loaded from {csv_file}")
+
+    def load_production_countries(self, csv_file):
+        query = """
+        LOAD CSV WITH HEADERS FROM $csvFile AS row
+        MATCH (m:Movie {tmdbId: toInteger(row.tmdbId)})  // Check if the movie exists
+        WITH m, row
+        MERGE (c:Country {country_code: row.country_code})
+        ON CREATE SET c.country_name = row.country_name
+        MERGE (m)-[:PRODUCED_IN]->(c);
+        """
+        with self.driver.session() as session:
+            session.run(query, csvFile=f'{csv_file}')
+            print(f"Production countries and relationships to movies loaded from {csv_file}")
+
+    def load_spoken_languages(self, csv_file):
+        query = """
+        LOAD CSV WITH HEADERS FROM $csvFile AS row
+        MATCH (m:Movie {tmdbId: toInteger(row.tmdbId)})  // Check if the movie exists
+        WITH m, row
+        MERGE (l:SpokenLanguage {language_code: row.language_code})
+        ON CREATE SET l.language_name = row.language_name
+        MERGE (m)-[:HAS_LANGUAGE]->(l);
+        """
+        with self.driver.session() as session:
+            session.run(query, csvFile=f'{csv_file}')
+            print(f"Spoken languages and relationships to movies loaded from {csv_file}")
+
+    def load_keywords(self, csv_file):
+        query = """
+        LOAD CSV WITH HEADERS FROM $csvFile AS row
+        MATCH (m:Movie {tmdbId: toInteger(row.tmdbId)})  // Check if the movie exists
+        SET m.keywords = row.keywords;
+        """
+        with self.driver.session() as session:
+            session.run(query, csvFile=f'{csv_file}')
+            print(f"Keywords loaded from {csv_file}")
+
+    def load_person(self, csv_file, role):
+        if role == "cast":
+            query = """
+            LOAD CSV WITH HEADERS FROM $csvFile AS row
+            MATCH (m:Movie {tmdbId: toInteger(row.tmdbId)})  // Check if the movie exists
+            WITH m, row
+            MERGE (p:Person {person_id: toInteger(row.actor_id), role: 'actor'})
+            ON CREATE SET p.name = row.name, p.gender = toInteger(row.gender)
+            MERGE (p)-[:ACTED_IN {character: coalesce(row.character, "None"), cast_id: toInteger(row.cast_id)}]->(m);
+            """
+        elif role == "crew":
+            query = """
+            LOAD CSV WITH HEADERS FROM $csvFile AS row
+            MATCH (m:Movie {tmdbId: toInteger(row.tmdbId)})  // Check if the movie exists
+            WITH m, row
+            MERGE (p:Person {person_id: toInteger(row.crew_id), role: row.job})
+            ON CREATE SET p.name = row.name, p.gender = toInteger(row.gender)
+            WITH p, m, row
+            CALL apoc.do.case([
+                row.job = 'Director', 'MERGE (p)-[:DIRECTED]->(m)',
+                row.job = 'Producer', 'MERGE (p)-[:PRODUCED]->(m)'
+            ],
+            'RETURN p, m', {p:p, m:m}) YIELD value
+            RETURN value.p, value.m;
+            """
+        with self.driver.session() as session:
+            session.run(query, csvFile=f'{csv_file}')
+            print(f"People loaded from {csv_file} as {role}")
+
+
+    # def load_links(self, csv_file):
+    #     query = """
+    #     LOAD CSV WITH HEADERS FROM $csvFile AS row
+    #     MATCH (m:Movie {tmdbId: toInteger(row.tmdbId)})  // Check if the movie exists
+    #     SET m.movieId = toInteger(row.movieId),
+    #         m.imdbId = row.imdbId;
+    #     """
+    #     with self.driver.session() as session:
+    #         session.run(query, csvFile=f'{csv_file}')
+    #         print(f"Links loaded from {csv_file}")
+
+
+    # def load_ratings(self, csv_file):
+    #     query = """
+    #     LOAD CSV WITH HEADERS FROM $csvFile AS row
+    #     MATCH (m:Movie {movieId: toInteger(row.movieId)})  // Check if the movie exists
+    #     WITH m, row
+    #     MERGE (p:Person {user_id: toInteger(row.userId), role: 'user'})
+    #     MERGE (p)-[r:RATED]->(m)
+    #     SET r.rating = toFloat(row.rating), r.timestamp = toInteger(row.timestamp);
+    #     """
+    #     with self.driver.session() as session:
+    #         session.run(query, csvFile=f'{csv_file}')
+    #         print(f"Ratings loaded from {csv_file}")
+
+
+
+def main():
+    uri = os.getenv('NEO4J_URI')
+    user = os.getenv('NEO4J_USER')
+    password = os.getenv('NEO4J_PASSWORD')
+
+    graph = CreateGraph(uri, user, password)
+
+    graph.create_constraints()
+
+    # Load data from CSV files with a limit on entries for movies
+    movie_limit = 15000  # Limit only applied to movies
+    graph.load_movies('https://storage.googleapis.com/movies-packt/normalized_movies.csv', movie_limit)
+
+    # Load related nodes and create relationships conditionally
+    graph.load_genres('https://storage.googleapis.com/movies-packt/normalized_genres.csv')
+    graph.load_production_companies('https://storage.googleapis.com/movies-packt/normalized_production_companies.csv')
+    graph.load_production_countries('https://storage.googleapis.com/movies-packt/normalized_production_countries.csv')
+    graph.load_spoken_languages('https://storage.googleapis.com/movies-packt/normalized_spoken_languages.csv')
+    graph.load_keywords('https://storage.googleapis.com/movies-packt/normalized_keywords.csv')
+    graph.load_person('https://storage.googleapis.com/movies-packt/normalized_cast.csv', role="cast")
+    graph.load_person('https://storage.googleapis.com/movies-packt/normalized_crew.csv', role="crew")
+    # graph.load_links('https://storage.googleapis.com/movies-packt/normalized_links_small.csv')
+    # graph.load_ratings('https://storage.googleapis.com/movies-packt/normalized_ratings_small.csv')
+    # graph.load_links('https://storage.googleapis.com/movies-packt/normalized_links.csv')
+    # graph.load_ratings('https://storage.googleapis.com/movies-packt/normalized_ratings.csv')
+
+
+    graph.close()
+
+if __name__ == "__main__":
+    main()
