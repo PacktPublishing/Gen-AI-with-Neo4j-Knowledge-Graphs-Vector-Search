b09b5f8cb6a8e02efc2e2099d5c0b3c91e6e56dc
Ravindranatha Anthapu <40771245+ravi-anthapu@users.noreply.github.com>
2024-12-01 17:18:19 +0530

    Update OpenAIEmbeddingModelService.java

--- ch10/springai_graphaugment/src/main/java/com/packt/genai/hnm/springai/graphaugment/service/OpenAIEmbeddingModelService.java
+++ ch10/springai_graphaugment/src/main/java/com/packt/genai/hnm/springai/graphaugment/service/OpenAIEmbeddingModelService.java
@@ -3,15 +3,17 @@ package com.packt.genai.hnm.springai.graphaugment.service;
 
 import org.springframework.ai.embedding.Embedding;
 import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class OpenAIEmbeddingModelService {
-    EmbeddingModel embeddingModel ;
+    private EmbeddingModel embeddingModel ;
 
-    public OpenAIEmbeddingModelService(org.springframework.ai.embedding.EmbeddingModel embeddingModel) {
+    @Autowired
+    public OpenAIEmbeddingModelService(EmbeddingModel embeddingModel) {
         this.embeddingModel = embeddingModel;
     }
 
