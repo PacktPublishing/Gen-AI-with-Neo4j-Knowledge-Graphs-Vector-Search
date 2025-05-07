5304c07e2e6ec97392a91f7ec2d4ca00d46e44bd
Rahul Limbachiya <89634175+rahul-packt@users.noreply.github.com>
2025-05-07 15:19:17 +0530

    Delete

--- ch10/langchain_graphaugment/src/main/java/com/packt/genai/hnm/langchain/graphaugment/service/IRequest.java
+++
@@ -1,7 +0,0 @@
-package com.packt.genai.hnm.langchain.graphaugment.service;
-
-public interface IRequest {
-    public String getCurStatus() ;
-
-    public boolean isComplete() ;
-}
--- ch10/springai_graphaugment/src/main/java/com/packt/genai/hnm/springai/graphaugment/service/IRequest.java
+++
@@ -1,7 +0,0 @@
-package com.packt.genai.hnm.springai.graphaugment.service;
-
-public interface IRequest {
-    public String getCurStatus() ;
-
-    public boolean isComplete() ;
-}
